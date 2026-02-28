bdbbcbb13900185581eb7a96473b7b1d680e104f
msdconsultores <administracion@msdconsultores.com>
2026-02-28 08:58:45 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
-  base: "/NOMBRE-REPO/",
+  base: "/msdconsultores-0de8dc90/",
   plugins: [react()],
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
